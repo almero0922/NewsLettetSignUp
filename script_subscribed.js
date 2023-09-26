00117803434f8c5d0eda9d217b74938ae20c31f4
almero0922 <albertomedel29@gmail.com>
2023-09-26 18:40:09 -0500

    Challenge Done

--- script_subscribed.js
+++ script_subscribed.js
@@ -1,3 +1,11 @@
 const email_text = document.getElementById("email_sub");
+const ca_dismiss = document.getElementById("ca-dismiss");
+email_text.innerText = localStorage.getItem("email");
 
-email_text.innerText = localStorage.getItem('email');
+function redirectPage(url) {
+  location.href = url;
+}
+
+ca_dismiss.addEventListener("click", (e) => {
+  redirectPage("./index.html");
+});
